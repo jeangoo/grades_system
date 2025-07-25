3c1f634b548501dfc64220b0a38689ee83bfad86
Jean <jeaancast03@gmail.com>
2025-07-25 11:40:00 -0400

    new changes(view function, validation, etc)

--- program/logic.py
+++ program/logic.py
@@ -1,12 +1,15 @@
+import time
+
 def save_grades():
     grades = []
     name, lastname = input("Enter your name and lastname: \n").capitalize().split()
     while True:
         try:
             grade = float(input("Enter your grade: "))
+            grades.append(grade)
         except ValueError:
             print("Please enter a valid float.")
-        grades.append(grade)
+
         if len(grades) == 3:
             print("Data saved correctly")
             break
@@ -15,3 +18,31 @@ def save_grades():
     
     with open("program/data.txt", "a+") as file:
         file.write(f"Name: {name} | Grades: {grades} | Average: {average:2f}\n")
+    
+    is_approved(average)
+
+def is_approved(average):
+    if average > 70:
+        print("Congrats!, you are approve.")
+    else:
+        print("Im sorry, you are not approve.")
+
+
+def view_grades():
+    name = input("What grades do you want to see? \n").capitalize()
+    try:
+        with open("program/data.txt", "r", encoding="utf-8") as     file:
+            found = False
+            for line in file:
+                if f"Name: {name}" in line:
+                    print("Fetching...")
+                    time.sleep(1.5)
+                    print("Grades found.")
+                    print(line.strip())
+                    found = True
+                    break
+                if not found:
+                    print("Student not found.")
+    except FileNotFoundError:
+        print("The file does not exist.")
+
--- program/main.py
+++ program/main.py
@@ -1,8 +1,23 @@
-from logic import save_grades
+from logic import save_grades, view_grades
+import time
 
 def main():
     print("Hi, follow the instructions and your grades are going to be saved in th efile data.txt\n")
-    save_grades()
+    while True:
+        print("what do you want to do? \n")
+        print("1. Save grades 2. View grades 3. Exit")
+        choice = input(" ")
+        if choice == "1":
+            save_grades()
+        elif choice == "2":
+            view_grades()
+        elif choice == "3":
+            print("Closing..")
+            time.sleep(1)
+            break
+        else:
+            print("Please enter a valid option.")
+
 
 if __name__ == "__main__":
     main()
